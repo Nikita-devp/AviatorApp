6625a52343c5df9ea09df6aeeec7b6205d2c0e81
Nikita <goyshik.nikita02@gmail.com>
2026-04-15 18:19:05 +0300

    fix red nuber

--- server.js
+++ server.js
@@ -247,6 +247,16 @@ app.use(cors({
 												socket.emit("history", history);
 											});
 											
+											socket.on("crash", (m) => {
+  setMultiplier(m);
+  setIsCrashed(true);
+
+  setTimeout(() => {
+    setIsCrashed(false);
+    setMultiplier(1.0);
+  }, 3000);
+});
+											
 											socket.on("cancelBet", async () => {
 												user.nextBet = 0;
 												await user.save();
