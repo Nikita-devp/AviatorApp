f3ed7fe41a98c72c848066a6d7143804dd71bdb1
Nikita <goyshik.nikita02@gmail.com>
2026-06-22 19:37:53 +0300

    делаем админку

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   balance: { type: Number, default: 1000 },
   bet: { type: Number, default: 0 },
   nextBet: { type: Number, default: 0 },
-  cashedOut: { type: Boolean, default: false }
+  cashedOut: { type: Boolean, default: false },
   role: {
     type: String,
     enum: ["user", "admin"],
