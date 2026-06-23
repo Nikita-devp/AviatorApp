c1e1db6bad181824e838b69925d9b16db769c22f
Nikita <goyshik.nikita02@gmail.com>
2026-06-24 01:43:25 +0300

    вывод пользователей

--- api/admin.js
+++ api/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
 const auth = require("../middlewares/auth");
 const admin = require("../middlewares/admin");
@@ -10,6 +11,26 @@ router.get("/", auth, admin, (req, res) => {
     username: req.user.username,
     role: req.user.role
   });
+});
+
+router.get("/users", auth, admin, async (req, res) => {
+  try {
+
+    const users =
+      await User.find(
+        {},
+        "username balance role"
+      );
+
+    res.json(users);
+
+  } catch {
+
+    res.status(500).json({
+      error:"Server error"
+    });
+
+  }
 });
 
 module.exports = router;
