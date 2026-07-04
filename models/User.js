18216f5d119164d3d5b7b670c7cb46dd92a3c518
Nikita <goyshik.nikita02@gmail.com>
2026-07-04 18:08:10 +0300

    переделываю под новый фунционал

--- models/User.js
+++ models/User.js
@@ -1,17 +1,48 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: String,
+const userSchema = new mongoose.Schema(
+{
+  username: {
+    type: String,
+    unique: true
+  },
+
   password: String,
-  balance: { type: Number, default: 1000 },
-  bet: { type: Number, default: 0 },
-  nextBet: { type: Number, default: 0 },
-  cashedOut: { type: Boolean, default: false },
+
+  balance: {
+    type: Number,
+    default: 1000
+  },
+
+  bet: {
+    type: Number,
+    default: 0
+  },
+
+  nextBet: {
+    type: Number,
+    default: 0
+  },
+
+  cashedOut: {
+    type: Boolean,
+    default: false
+  },
+
   role: {
     type: String,
     enum: ["user", "admin"],
     default: "user"
+  },
+
+  isBanned: {
+    type: Boolean,
+    default: false
   }
+
+},
+{
+  timestamps: true
 });
 
 module.exports = mongoose.model("User", userSchema);
